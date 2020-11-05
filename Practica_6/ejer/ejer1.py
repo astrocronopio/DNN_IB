d1636886808d097c5ddbb8d67bb2e8a34800a8ae
astrocronopio@hotmail.com <Evelyn  Coronel>
2020-11-05 01:07:30 -0300

    Hasta el punto f del 6.

--- Practica_6/ejer/ejer1.py
+++ Practica_6/ejer/ejer1.py
@@ -1,7 +1,10 @@
-from sklearn import tree
-import pandas 
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier,plot_tree
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor, RandomForestRegressor
 
+from sklearn.model_selection import GridSearchCV
+import pandas 
 import numpy as np
+import graphviz 
 
 import matplotlib.pyplot as plt
 
@@ -15,45 +18,197 @@ mpl.rcParams.update({
 
 # En primer lugar, a partir de la variable Sales creen una nueva variable binaria que se llame High, que sea Yes si Sales es mayor a 8 o No si es menor que 8.  A partir de ahí tienen que  usar  árboles  de  decisión  para  estimar High con  clasificación  y Sales con  regresión(importante: tomar la precaución de sacar una al estimar la otra, ya que si no tendrán errores artificialmente bajos)
 
-#"Sales","CompPrice","Income","Advertising","Population","Price","ShelveLoc","Age","Education","Urban","US"
+# "Sales","CompPrice","Income","Advertising",
+# "Population","Price","ShelveLoc","Age",
+# "Education","Urban","US"
 
-# (a)  Separar los datos en dos particiones, una para datos de entrenamiento/validación (osea, de desarrollo) y otra para test.
+class arbol_decision(object):
+    def __init__(self, type_tree):
+        self.tree = type_tree
+        
+    def train_tree(self, X, y):
+        self.tree=self.tree.fit(X,y)
+        
+    def test_tree(self, X_test):
+        return self.tree.predict(X_test)
+    
+    def plot_save_tree(self, val_y, y_pred, plot_path):
+        names= ["CompPrice","Income","Advertising",
+        "Population","Precio","Posición en vitrina","Edad",
+        "Education","Urban","US"]
+        plt.figure(12)
+        plot_tree(self.tree, filled=True, feature_names=names, rounded=True, fontsize=10,
+        class_names=["Por encima  de 8", "Por debajo de 8"])
+        plt.figure(5)
+        plt.xlabel("Dato")
+        plt.ylabel("Predicho")
+        plt.scatter(val_y, y_pred, alpha=0.6)
+        plt.plot(val_y, val_y, color='black', alpha=0.6)
+        plt.savefig(plot_path)
+        plt.show()
+
 def data_loading():
     df = pandas.read_csv("./Carseats.csv", header='infer')
+    df.replace(('Yes', 'No'), (1, 0), inplace=True)
+    df.replace(('Good', 'Medium', 'Bad'), (3, 2, 1), inplace=True)
+    
     df = df.to_numpy()
     sales = df[:,0]
-    print(sales[:10])
-    print(type(sales))
-    print(sales.shape)
+    high = np.array([[ 1 if sale>8 else 0 for sale in sales]]).T
+    df = np.concatenate((df, high), axis=1 )
+    return df
+
+# (a)  Separar los datos en dos particiones, una para datos de entrenamiento/validación (osea, de desarrollo) y otra para test.
+
+# Podria haber usado sklearn.model_selection.train_test_split
+# me siento una boluda uwu
+def split_data(train, val, df):   
+    train= int(400*train)
+    val= int(400*val)
+
+    df_train = df[:train ,:]
+    df_val = df[-val:,:]
     
-    high = np.heaviside(sales, 0)
-    print(sales[:10], high[:10] )
+    return df_train, df_val
+
+def separate_data(data):
+    return  data[:,1:-2], data[:,0], data[:,-1]
+
+
+def item_B(train_x, train_y, val_x, val_y, plot_option=False):
+    B_tree = arbol_decision(DecisionTreeClassifier(max_depth=2))
+    B_tree.train_tree(train_x, train_y)
+    y_pred = B_tree.test_tree(val_x)
+    
+    if plot_option==True:
+        B_tree.plot_save_tree(val_y, y_pred, "B_tree.pdf")
+         
+    print("Error <B_tree>: {} errores de {}. {}%, acc:{}, val_acc:{}".format(                                                                  
+            np.sum(val_y-y_pred), len(val_y), 
+            100*np.sum(val_y-y_pred)/len(val_y),
+            B_tree.tree.score(train_x, train_y),
+            B_tree.tree.score(val_x, val_y)))
+    
+def item_C(train_x, train_y, val_x, val_y, plot_option=False):
+    C_tree = arbol_decision(DecisionTreeRegressor(max_depth=2))
+    C_tree.train_tree(train_x, train_y)
+    y_pred = C_tree.test_tree(val_x)
+
+    if plot_option==True:
+        C_tree.plot_save_tree(val_y, y_pred, "C_tree.pdf")
+
+    print("Error <C_tree>: {} de error de {} datos.  acc:{}, val_acc:{}".format(
+        np.sum(val_y-y_pred), len(val_y),
+        C_tree.tree.score(train_x, train_y),
+        C_tree.tree.score(val_x, val_y)))
+    
+    parameters = {'max_depth':range(2,20)}
+    clf = GridSearchCV(DecisionTreeRegressor(), parameters)
+    clf.fit(X=train_x, y=train_y)
+    tree_model = clf.best_estimator_
+    print (clf.best_score_, clf.best_params_) 
+    
+def item_D(train_x, train_y, val_x, val_y, plot_option=False):
+    D_tree = DecisionTreeRegressor(random_state=0)
+    parameters = D_tree.cost_complexity_pruning_path(train_x, train_y)
+    ccp_alphas, impurities = parameters.ccp_alphas, parameters.impurities
+
+    regressor_forest = []
+    for ccp_alpha in ccp_alphas:
+        regressor_tree = DecisionTreeRegressor(random_state=0, ccp_alpha=ccp_alpha)
+        regressor_tree.fit(train_x, train_y)
+        regressor_forest.append(regressor_tree)
+    
+    nodo_per_tree= [arbol.tree_.node_count for arbol in regressor_forest]
+    max_depth = [arbol.tree_.max_depth for arbol in regressor_forest]
+    
+    train_scores = [arbol.score(train_x, train_y) for arbol in regressor_forest]
+    test_scores = [arbol.score(val_x, val_y) for arbol in regressor_forest]
     
     
-data_loading()
+    fig,ax = plt.subplots()
+    ax.set_ylabel("accuracy")
+    ax.set_xlabel("ccp $\\alpha$")
+    ax.plot(ccp_alphas[:-2], train_scores[:-2], marker='o', label="train",drawstyle="steps-post", color="blue", alpha=0.7)
+    ax.plot(ccp_alphas[:-2], test_scores[:-2], marker='s', label="val",drawstyle="steps-post", color="red", alpha=0.7)
+    ax.legend(loc=0)
+    ax2=ax.twinx()
+    ax2.plot(ccp_alphas[:-2], max_depth[:-2], marker='^', label="profundidad",drawstyle="steps-post", color="green", alpha=0.7)
+    ax2.set_ylabel("Profundidad")
+    ax2.legend(loc='center right')
     
+    
+    D_tree_optimo = arbol_decision(DecisionTreeRegressor(max_depth=5, ccp_alpha=0.25))
+    D_tree_optimo.train_tree(train_x, train_y)
+    y_pred = D_tree_optimo.test_tree(val_x)
 
-def split_data():
-    pass
-
-# (b)  Entrenar un árbol de decisión para clasificación de la variableHigh. Hacer un plot delárbol (usarscikit-learn) e interpretar los resultados.
-
-def train_tree_high():
-    pass
-# (c)  Entrenar un árbol de decisión para regresión de la variableSales.  Hacer un plot delárbol (usarscikit-learn) e interpretar los resultados.
-
-def  train_tree_Sales():
-    pass
-
-# (d)  ¿Cuál es el error de test que obtienen en cada caso?  Comparar con el error de entrenamiento y determinar si tienen overfitting o no.
+    if plot_option==True:
+        D_tree_optimo.plot_save_tree(val_y, y_pred, "D_tree_optimo.pdf")
+        
+    plt.show()
+    
+    print("Error <D_tree>: {} de error de {} datos.  acc:{}, val_acc:{}".format(
+        np.sum(val_y-y_pred), len(val_y), 
+        D_tree_optimo.tree.score(train_x, train_y),
+        D_tree_optimo.tree.score(val_x, val_y)))
 
 
+def item_F(train_x, train_y, val_x, val_y, plot_option=False):
+    F_tree = arbol_decision(BaggingRegressor(n_estimators=8, random_state=0))
+    FF_tree = arbol_decision(DecisionTreeRegressor(max_depth=1, random_state=0))
+    F_tree.train_tree(train_x, train_y)
+    y_pred = F_tree.test_tree(val_x)
 
-# (e)  Para el árbol de regresión, usarcross-validationpara determinar el nivel óptimo de com-plejidad del árbol.  Busquen cómo usar enscikit-learnla técnica depruningparamejorar la tasa de error de test.
+    FF_tree.train_tree(train_x, train_y)
+    yFF_pred = FF_tree.test_tree(val_x)
 
-# (f)  Para el caso de regresión,  usar el abordaje tipobaggingpara mejorar el error de test.Comparar con el abordaje de un único árbol de decisión.  Buscar enscikit-learncómo determinar el orden de importancia de los atributos.
+    plt.figure(5)
+    plt.xlabel("Dato")
+    plt.ylabel("Predicho")
+    plt.scatter(val_y, y_pred, alpha=0.6)
+    plt.scatter(val_y, yFF_pred, c='red', alpha=0.6)
+    
+    plt.plot(val_y, val_y, color='black', alpha=0.6)
+    plt.savefig("F_tree.pdf")
+    plt.show()
+                 
+    print("Error <F_tree>: {} errores de {}. {}%, acc:{}, val_acc:{}".format(                                                                  
+            np.sum(val_y-y_pred), len(val_y), 
+            100*np.sum(val_y-y_pred)/len(val_y),
+            F_tree.tree.score(train_x, train_y),
+            F_tree.tree.score(val_x, val_y)))
+    
+    print("Error <FF_tree>: {} errores de {}. {}%, acc:{}, val_acc:{}".format(                                                                  
+        np.sum(val_y-yFF_pred), len(val_y), 
+        100*np.sum(val_y-yFF_pred)/len(val_y),
+        FF_tree.tree.score(train_x, train_y),
+        FF_tree.tree.score(val_x, val_y)))
+            
+def main():    
+    data = data_loading()        
+    train, val = split_data(0.8,0.2, data)
+
+    train_split, train_sales, train_high = separate_data(train)
+    val_split, val_sales, val_high = separate_data(val)
+    # (b)  Entrenar un árbol de decisión para clasificación de la variable High. Hacer un plot delárbol (usarscikit-learn) e interpretar los resultados.
+    # item_B(train_split, train_high, val_split, val_high )
+        
+    # (c)  Entrenar un árbol de decisión para regresión de la variableSales.  Hacer un plot delárbol (usarscikit-learn) e interpretar los resultados.
+    # item_C(train_split, train_sales, val_split, val_sales )
+
+    # (d)  ¿Cuál es el error de test que obtienen en cada caso?  Comparar con el error de entrenamiento y determinar si tienen overfitting o no.
+    # (e)  Para el árbol de regresión, usarcross-validationpara determinar el nivel óptimo de com-plejidad del árbol.  Busquen cómo usar enscikit-learnla técnica depruningparamejorar la tasa de error de test.
+    #item_D(train_split, train_sales, val_split, val_sales, True)
+
+    # (f)  Para el caso de regresión,  usar el abordaje tipobaggingpara mejorar el error de test.Comparar con el abordaje de un único árbol de decisión.  Buscar enscikit-learncómo determinar el orden de importancia de los atributos.
+    
+    item_F(train_split, train_high, val_split, val_high, True )
 
 # (g)  Usarrandom forestspara mejorar los resultados dados. Comparar el error de test con los abordajes anteriores.  ¿Cambia el orden de la importancia de los atributos?  Hacer unplot con el error de test en función del del hiperparámetromax_featuresque limitael número de atributos a incluir en cada split.  Hacer otro plot equivalente en funcióndemax_depth.1
 
 
 # (h)  Hacer la misma regresión usandoAdaBoosty comparar errores de test con lo obtenidoconRandom Foresten el punto anterior.2
+
+if __name__ == '__main__':
+    main()
+    
